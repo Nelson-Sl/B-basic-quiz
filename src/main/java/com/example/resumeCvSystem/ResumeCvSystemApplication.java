df868b468d27a7c52ed316a29588d934f62c322e
Shen Liao <shen.liao@thoughtworks.com>
2020-09-15 12:08:39 +0800

    [Nelson Liao] feat: create empty project

---
+++ src/main/java/com/example/resumeCvSystem/ResumeCvSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumeCvSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeCvSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeCvSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resumeCvSystem/ResumeCvSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resumeCvSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeCvSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
