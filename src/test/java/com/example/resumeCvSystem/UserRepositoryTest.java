5b4e157e26dad17423a609d19d1e75c4bd38d289
Shen Liao <shen.liao@thoughtworks.com>
2020-09-23 20:35:26 +0800

    [Nelson Liao] feat: refactor user repository test & delete unused user repo function

---
+++ src/test/java/com/example/resumeCvSystem/UserRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.example.resumeCvSystem;
+
+import com.example.resumeCvSystem.entity.UserEntity;
+import com.example.resumeCvSystem.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @BeforeEach
+    public void setUp() {
+        entityManager.persistAndFlush(UserEntity.builder()
+                .age(27L)
+                .name("Nelson")
+                .avatar("http://www.nelson.org/nelson.jpg")
+                .description("A handsome boy")
+                .build()
+        );
+    }
+
+    @Test
+    void should_return_user_when_id_exists() {
+        Optional<UserEntity> foundUser = userRepository.findById(1L);
+
+        assertThat(foundUser.isPresent()).isEqualTo(true);
+        assertThat(foundUser.get()).isEqualTo(UserEntity.builder()
+                .id(1L)
+                .age(27L)
+                .name("Nelson")
+                .avatar("http://www.nelson.org/nelson.jpg")
+                .description("A handsome boy")
+                .build());
+    }
+
+    @Test
+    void should_return_empty_when_id_not_exist() {
+        Optional<UserEntity> foundUser = userRepository.findById(5L);
+
+        assertThat(foundUser.isPresent()).isEqualTo(false);
+    }
+
+}
